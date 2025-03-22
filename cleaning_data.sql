d80da8ce9742d724382dac9f104d5906c3d72fed
John Ndungu <ndungulink@gmail.com>
2025-03-22 08:38:59 +0300

    Removing the Nulls

--- cleaning_data.sql
+++ cleaning_data.sql
@@ -119,7 +119,47 @@ MODIFY COLUMN `date` DATE;
 
 
 
+-- Rmoving the Nulls
 
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+UPDATE layoffs_staging2
+SET  industry = NULL 
+WHERE industry = "";
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+
+SELECT * 
+FROM  layoffs_staging2
+WHERE company LIKE "Ball";
+
+SELECT t1.industry, t2.industry
+FROM  layoffs_staging2 t1
+JOIN layoffs_staging2  t2
+	ON t1.company  = t2.company
+    AND t1.location = t2.location
+WHERE (t1.industry IS  NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2  t2
+	ON t1.company  = t2.company
+SET  t1.industry  = t2.industry
+WHERE t1.industry IS  NULL 
+AND t2.industry IS NOT NULL;
+
+select *
+FROM layoffs_staging2;
+
+-- END OF REMOVING NULLS
 
 
 
